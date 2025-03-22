61be628fd3e6071db93cf169dfc73bb17f0e7bcb
Jimmy Tran <jtran2@academic.rrc.ca>
2025-03-21 22:22:54 -0500

    edit: documentation for branchModel

--- src/api/v1/models/branchModel.ts
+++ src/api/v1/models/branchModel.ts
@@ -1,6 +1,25 @@
 /**
  * @interface Branch
  * @description Represents a branch object
+ *
+ * @openapi
+ * components:
+ *   schemas:
+ *     Branch:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for a branch
+ *         name:
+ *           type: string
+ *           description: Name of a branch
+ *         address:
+ *           type: string
+ *           description: Address of a branch
+ *         phone:
+ *           type: string
+ *           description: Phone number of a branch
  */
 export type Branch = {
   id: string;
