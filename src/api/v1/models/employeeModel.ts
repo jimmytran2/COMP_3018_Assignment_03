16780d49f1e94b8dd44f60aa3f8afcee40b2425d
Jimmy Tran <jtran2@academic.rrc.ca>
2025-03-21 22:29:30 -0500

    edit: documentation for employeeModel

--- src/api/v1/models/employeeModel.ts
+++ src/api/v1/models/employeeModel.ts
@@ -1,6 +1,35 @@
 /**
  * @interface Employee
  * @description Represents an employee object
+ *
+ * @openapi
+ * components:
+ *   schemas:
+ *     Employee:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for an employee
+ *         name:
+ *           type: string
+ *           description: Name of an employee
+ *         position:
+ *           type: string
+ *           description: Job position of an employee
+ *         department:
+ *           type: string
+ *           description: Department where an employee works
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: Email address of an employee
+ *         phone:
+ *           type: string
+ *           description: Phone number of an employee
+ *         branch:
+ *           type: string
+ *           description: Unique identifier of the branch the employee works at
  */
 export type Employee = {
   id: string;
